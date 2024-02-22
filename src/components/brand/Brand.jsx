b7d49be17d62f3edf87c532b387314afb1b3a157
moha_01 <mm7.mansour@gmail.com>
2024-02-22 08:50:54 -0800

    A react project to practice usage

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './brand.css';
+import { google, slack, atlassin, dropbox, shopify } from './imports';
+
+const Brand = () => {
+  return (
+    <div className='gpt3__brand section__padding'>
+      <div><img src={google} alt="Google" /></div>
+      <div><img src={slack} alt="Slack" /></div>
+      <div><img src={atlassin} alt="Atlassin" /></div>
+      <div><img src={dropbox} alt="Dropbox" /></div>
+      <div><img src={shopify} alt="Shopify" /></div>
+    </div>
+  )
+}
+
+export default Brand
